666eb90d8ab3eb7ebb609d313c3ae7e6fcf5c4b1
yqCherish <yq_cherish@qq.com>
2017-07-19 11:15:49 +0800

    back

--- config/index.js
+++ config/index.js
@@ -12,7 +12,7 @@ module.exports = {
 
     //本地
     assetsSubDirectory: './static',
-    assetsPublicPath: '/',
+    assetsPublicPath: './',
     productionSourceMap: true,
     // Gzip off by default as many popular static hosts such as
     // Surge or Netlify already gzip all static assets for you.
